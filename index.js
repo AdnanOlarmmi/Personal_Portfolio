8e3adb14f80ef3ac32d672f6a40b920a64907476
AdnanOlarmmi <olarmmi@gmail.com>
2022-09-08 10:29:46 +0100

    Finished with the pop up window

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const navTriggerEl = document.querySelector('.burger');
 const navEl = document.querySelector('.nav');
 const navItems = document.querySelectorAll('.nav__items');
 const navUntriggerEl = document.querySelector('.X-Icon');
-const popUpWindow = document.querySelector(".popUpWindow");
-const projects = document.querySelector(".projects");
+const popUpWindow = document.querySelector('.popUpWindow');
+const projects = document.querySelector('.projects');
 const PopUntriggerEl = document.getElementsByClassName('Xpop');
-const seeProject = document.getElementsByClassName("seeProject");
-const projectdetails = [{name:'Multi-post story', description:'Doe', featuredImage:'images/ImgPlaceholder.svg', technologies:['html','css','bootstrap','ruby'], linkLiveVersion:'', linkVersion:''},{name:'Stories Story story', description:'Doe', featuredImage:'images/ImgPlaceholder.svg', technologies:['html','css','bootstrap','ruby'], linkLiveVersion:'', linkVersion:''},{name:'Story Stories story', description:'Doe', featuredImage:'images/ImgPlaceholder.svg', technologies:['html','css','bootstrap','ruby'], linkLiveVersion:'', linkVersion:''},{name:'Story Story stories', description:'Doe', featuredImage:'images/ImgPlaceholder.svg', technologies:['html','css','bootstrap','ruby'], linkLiveVersion:'', linkVersion:''}];
+const seeProject = document.getElementsByClassName('seeProject');
+const projectdetails = [
+  {
+    name: 'Multi-post story', description: 'Doe', featuredImage: 'images/ImgPlaceholder.svg', technologies: ['html', 'css', 'bootstrap', 'ruby'], linkLiveVersion: '', linkVersion: '',
+  },
+  {
+    name: 'Stories Story story', description: 'Doe', featuredImage: 'images/ImgPlaceholder.svg', technologies: ['html', 'css', 'bootstrap', 'ruby'], linkLiveVersion: '', linkVersion: '',
+  },
+  {
+    name: 'Story Stories story', description: 'Doe', featuredImage: 'images/ImgPlaceholder.svg', technologies: ['html', 'css', 'bootstrap', 'ruby'], linkLiveVersion: '', linkVersion: '',
+  },
+  {
+    name: 'Story Story stories', description: 'Doe', featuredImage: 'images/ImgPlaceholder.svg', technologies: ['html', 'css', 'bootstrap', 'ruby'], linkLiveVersion: '', linkVersion: '',
+  },
+];
 
 function hideNav() {
   navTriggerEl.classList.remove('close');
@@ -32,9 +45,9 @@ function toggleNav() {
   });
 }
 
-const renderPopUp = _ => {
+const renderPopUp = (id) => {
   const markup = `<div class="popUpwindow__head flex-row">
-    <p>Multi-post story</p>
+    <p>${projectdetails[id].name}</p>
     <img
       src="images/X-Icon.svg"
       alt="X-Icon"
@@ -92,33 +105,32 @@ const renderPopUp = _ => {
     /></a>
   </div>`;
 
-popUpWindow.innerHTML = markup;
-
-}
+  popUpWindow.innerHTML = markup;
+  for (let i = 0; i < PopUntriggerEl.length; i + 1) {
+    PopUntriggerEl[i].addEventListener('click', () => {
+      popUpWindow.classList.remove('showPopUp');
+    });
+  }
+};
 
 // const renderStack = projectdetails.forEach((element,index) => {
 //    element.technologies[index]
 // });;
 
 function togglePopUp() {
-  
-for(i=0; i<PopUntriggerEl.length; i++){
-  PopUntriggerEl[i].addEventListener('click', ()=>{
-    popUpWindow.classList.remove('showPopUp')
-  })
+  for (let i = 0; i < seeProject.length; i + 1) {
+    seeProject[i].addEventListener('click', (e) => {
+      const projectId = e.target.id;
+      renderPopUp(projectId);
+      popUpWindow.classList.add('showPopUp');
+    });
+  }
 }
 
-for(i=0; i<seeProject.length; i++){
-  seeProject[i].addEventListener('click', ()=>{
-    popUpWindow.classList.add('showPopUp')
-  })
-}
-}
-
-const renderProjectDetails = _ => {
-  let markup = "";
-projectdetails.forEach((elem, index) => {
-  markup += `<div class="projects__item flex-column row${index+1}">
+const renderProjectDetails = () => {
+  let markup = '';
+  projectdetails.forEach((elem, index) => {
+    markup += `<div class="projects__item flex-column row${index + 1}">
   <img
     class="imgPlaceholder"
     src="${elem.featuredImage}"
@@ -133,29 +145,28 @@ projectdetails.forEach((elem, index) => {
       standard dummy text.
     </p>
     <ul class="projects__item-language">
-      <li class="projects__item-language-item">${elem.technologies[0]}</li>
-      <li>
-        <img src="images/Rectangle.svg" alt="straightLine" />
-      </li>
+    <li class="projects__item-language-item">${elem.technologies[0]}</li>
+<li>
+  <img src="images/Rectangle.svg" alt="straightLine" />
+</li>
 
-      <li class="projects__item-language-item">${elem.technologies[1]}</li>
-      <li>
-        <img src="images/Rectangle.svg" alt="straightLine" />
-      </li>
-      <li class="projects__item-language-item">${elem.technologies[2]}</li>
-      <li>
-        <img src="images/Rectangle.svg" alt="straightLine" />
-      </li>
-      <li class="projects__item-language-item">${elem.technologies[3]}</li>
+<li class="projects__item-language-item">${elem.technologies[1]}</li>
+<li>
+  <img src="images/Rectangle.svg" alt="straightLine" />
+</li>
+<li class="projects__item-language-item">${elem.technologies[2]}</li>
+<li>
+  <img src="images/Rectangle.svg" alt="straightLine" />
+</li>
+<li class="projects__item-language-item">${elem.technologies[3]}</li>
     </ul>
-    <a href="#" class="seeProject">See project</a>
+    <a href="#" class="seeProject" id=${index}>See project ${index}</a>
   </div>
 </div>`;
-})
+  });
 
-projects.innerHTML = markup;
-}
+  projects.innerHTML = markup;
+};
 toggleNav();
-renderPopUp();
 renderProjectDetails();
 togglePopUp();
